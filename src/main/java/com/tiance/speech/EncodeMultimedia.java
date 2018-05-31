7ac4b122c372ea5227db94518dd6f509a3a7ea24
tiance <tiance@yonyou.com>
2018-05-31 14:56:20 +0800

    提交出版语音识别接口
    Signed-off-by: tiance <tiance@yonyou.com>

---
+++ src/main/java/com/tiance/speech/EncodeMultimedia.java
@@ -0,0 +1,51 @@
+package com.tiance.speech;
+
+import java.io.File;
+
+import it.sauronsoftware.jave.AudioAttributes;
+import it.sauronsoftware.jave.Encoder;
+import it.sauronsoftware.jave.EncoderException;
+import it.sauronsoftware.jave.EncodingAttributes;
+import it.sauronsoftware.jave.InputFormatException;
+
+/**
+ * 对音频进行重编码
+ * 
+ * @author jtian
+ *
+ */
+public class EncodeMultimedia {
+	public static void main(String[] args) throws Exception {
+		File source = new File("d:\\audio\\android.mp3");
+		File target = new File("d:\\audio\\target.pcm");
+
+		mp3ToPcm(source, target);
+	}
+
+	/**
+	 * 
+	 * @param source
+	 *            源文件
+	 * @param target
+	 *            转换后文件
+	 * @throws IllegalArgumentException
+	 * @throws InputFormatException
+	 * @throws EncoderException
+	 */
+	public static void mp3ToPcm(File source, File target)
+			throws IllegalArgumentException, InputFormatException, EncoderException {
+		EncodingAttributes attrs = new EncodingAttributes();
+		attrs.setFormat("s16le"); // 设置目标文件的封装格式,使用pcm的一种封装格式s16le
+
+		// 设置目标语音的属性
+		AudioAttributes audio = new AudioAttributes();
+		audio.setCodec("pcm_s16le"); // 设置编码格式
+		audio.setSamplingRate(16000); // 设置采样率
+		audio.setChannels(1); // 设置声道数,即单声道
+		attrs.setAudioAttributes(audio);
+
+		Encoder encoder = new Encoder();
+		encoder.encode(source, target, attrs); // 开始转换
+		System.out.println("****mp3 转 pcm转换成功****");
+	}
+}
---
+++ src/main/java/com/tiance/speech/VoiceRecognize.java
@@ -0,0 +1,37 @@
+package com.tiance.speech;
+
+import org.json.JSONObject;
+
+import com.baidu.aip.speech.AipSpeech;
+
+/**
+ * 语音识别工具测试类
+ * 
+ * @author jtian
+ *
+ */
+public class VoiceRecognize {
+	// 从baidu云获取,设置APPID/AK/SK
+	public static final String APP_ID = "11318880";
+	public static final String API_KEY = "10FGV80epwTwo9EcPqIWRamc";
+	public static final String SECRET_KEY = "iDnODuyhkp9weYtrGTIXwWlt5KCsPbWR1";
+
+	public static void main(String[] args) {
+		// 初始化一个AipSpeech
+		AipSpeech client = new AipSpeech(APP_ID, API_KEY, SECRET_KEY);
+
+		// 可选：设置网络连接参数
+		client.setConnectionTimeoutInMillis(2000);
+		client.setSocketTimeoutInMillis(60000);
+
+		// 可选：设置log4j日志输出格式，若不设置，则使用默认配置
+		// 也可以直接通过jvm启动参数设置此环境变量
+		System.setProperty("aip.log4j.conf", "log4j.properties");
+
+		String file = "d:\\target.pcm";
+		// 调用接口asr(文件, 格式, 采样率, null)
+		JSONObject res = client.asr(file, "pcm", 16000, null);
+		System.out.println(res.toString(2));
+
+	}
+}
